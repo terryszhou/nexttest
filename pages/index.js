793cbb7a78fae7c27796d79c7ac1e40eea8031bc
terryzrestoic <terry@restoic.com>
2021-11-07 10:10:11 -0600

    Details component layout MVP

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
-import { Container, Flex, VStack } from '@chakra-ui/react'
+import { Container, Flex } from '@chakra-ui/react'
 import Cart from '/src/sections/cart.js'
 import Details from '/src/sections/details.js'
 
 
 const IndexPage = () => (
   <Container maxW='container.xl' p={0}>
-    <Flex h='100vh' py={20}>
-      <Cart />
+    <Flex h='100vh' py={10}>
       <Details />
+      <Cart />
     </Flex>
   </Container>
 )
